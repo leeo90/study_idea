0efccef901a1e208787c1bcf6298436711b140d1
wangning <1>
2019-07-02 15:04:11 +0800

    first commit

---
+++ manager-service/src/main/java/com/leeo/App.java
@@ -0,0 +1,13 @@
+package com.leeo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
